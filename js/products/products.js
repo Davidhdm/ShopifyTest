2976ebeef55d00caf7502a063a1628c350ee9efe
Davidhdm <david.hdm9@gmail.com>
2021-08-16 19:09:00 +0200

    github pages sliders img fix

--- js/products/products.js
+++ js/products/products.js
@@ -4,56 +4,56 @@ export const products = {
       "name": "Black Jacket",
       "brand": "Gucci",
       "price": "£000",
-      "img": "/img/products/new-in/black_jacket.png"
+      "img": "\\img\\products\\new-in\\black_jacket.png"
     },
     {
       "id": 2,
       "name": "White Jacket",
       "brand": "Bacon Clothing",
       "price": "£000",
-      "img": "/img/products/new-in/white_jacket.png"
+      "img": "\\img\\products\\new-in\\white_jacket.png"
     },
     {
       "id": 3,
       "name": "Yellow Jacket",
       "brand": "Annakiki",
       "price": "£000",
-      "img": "/img/products/new-in/yellow_jacket.png"
+      "img": "\\img\\products\\new-in\\yellow_jacket.png"
     },
     {
       "id": 4,
       "name": "Camouflage Jacket",
       "brand": "Black Crows",
       "price": "£000",
-      "img": "/img/products/new-in/camouflage_jacket.png"
+      "img": "\\img\\products\\new-in\\camouflage_jacket.png"
     },
     {
       "id": 5,
       "name": "Green Hoodie",
       "brand": "Blue Banana",
       "price": "£000",
-      "img": "/img/products/new-in/green_hoodie.png"
+      "img": "\\img\\products\\new-in\\green_hoodie.png"
     },
     {
       "id": 6,
       "name": "Jeans",
       "brand": "Hollister",
       "price": "£000",
-      "img": "/img/products/new-in/jeans.jpg"
+      "img": "\\img\\products\\new-in\\jeans.jpg"
     },
     {
       "id": 7,
       "name": "Winter Gloves",
       "brand": "Ejendals",
       "price": "£000",
-      "img": "/img/products/new-in/winter_gloves.jpg"
+      "img": "\\img\\products\\new-in\\winter_gloves.jpg"
     },
     {
       "id": 8,
       "name": "Skyblue Hoodie",
       "brand": "Blue Banana",
       "price": "£000",
-      "img": "/img/products/new-in/skyblue_hoodie.png"
+      "img": "\\img\\products\\new-in\\skyblue_hoodie.png"
     }
   ],
   "best_sellers": [{
@@ -61,56 +61,56 @@ export const products = {
       "name": "Pink Jacket",
       "brand": "Gucci",
       "price": "£000",
-      "img": "/img/products/best-sellers/pink_jacket.png"
+      "img": "\\img\\products\\best-sellers\\pink_jacket.png"
     },
     {
       "id": 2,
       "name": "Pink-Knit Beanie",
       "brand": "Bacon Clothing",
       "price": "£000",
-      "img": "/img/products/best-sellers/pink-knit_beanie.png"
+      "img": "\\img\\products\\best-sellers\\pink-knit_beanie.png"
     },
     {
       "id": 3,
       "name": "Gold Gloves",
       "brand": "Annakiki",
       "price": "£000",
-      "img": "/img/products/best-sellers/gold_gloves.png"
+      "img": "\\img\\products\\best-sellers\\gold_gloves.png"
     },
     {
       "id": 4,
       "name": "Cream Handbag",
       "brand": "Black Crows",
       "price": "£000",
-      "img": "/img/products/best-sellers/cream_handbag.png"
+      "img": "\\img\\products\\best-sellers\\cream_handbag.png"
     },
     {
       "id": 5,
       "name": "Red Pants",
       "brand": "Dickies",
       "price": "£000",
-      "img": "/img/products/best-sellers/red_pants.jpg"
+      "img": "\\img\\products\\best-sellers\\red_pants.jpg"
     },
     {
       "id": 6,
       "name": "Deep Blue O-Shirt",
       "brand": "True Story",
       "price": "£000",
-      "img": "/img/products/best-sellers/deep_blue_shirt.jpg"
+      "img": "\\img\\products\\best-sellers\\deep_blue_shirt.jpg"
     },
     {
       "id": 7,
       "name": "I Love NY Cap",
       "brand": "Clothing",
       "price": "£000",
-      "img": "/img/products/best-sellers/ilny_cap.png"
+      "img": "\\img\\products\\best-sellers\\ilny_cap.png"
     },
     {
       "id": 8,
       "name": "Sport Shorts",
       "brand": "Izas",
       "price": "£000",
-      "img": "/img/products/best-sellers/sport_shorts.jpg"
+      "img": "\\img\\products\\best-sellers\\sport_shorts.jpg"
     }
   ]
 }
